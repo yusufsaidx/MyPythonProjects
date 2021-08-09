a72ea1782e5be1a69079f23c7362049b2eaa5211
Yusuf Said Çöpcü <88095079+yusufsaidx@users.noreply.github.com>
2021-08-10 02:27:51 +0300

    Update and rename ytbot.py to LINKCLICKER.py

--- pythonbot/LINKCLICKER.py
+++ pythonbot/LINKCLICKER.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-link = input('link girin')
-driver = webdriver.Chrome("C:/Users/syusu/Desktop/chromedriver.exe")
+link = input('input link...')
+driver = webdriver.Chrome("your file path "CHROME WEB DRIVER")
 for a in range(10):
     for i in range(15):
         driver.get(link)
         print(i)
         time.sleep(2)
-        
