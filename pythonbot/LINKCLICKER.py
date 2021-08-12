3b331dfec33471fd154ec8525723e849099957d0
Yusuf Said Çöpcü <88095079+yusufsaidx@users.noreply.github.com>
2021-08-12 23:25:58 +0300

    changed to code

--- pythonbot/LINKCLICKER.py
+++ pythonbot/LINKCLICKER.py
@@ -4,7 +4,6 @@ import time
 link = input('input link...')
 driver = webdriver.Chrome("your file path "CHROME WEB DRIVER")
 for a in range(10):
-    for i in range(15):
         driver.get(link)
         print(i)
         time.sleep(2)
